e0e7f70f310f398a5e96aed7e803196ce1fd4bc0
Quebrant <agraval167@g.educaand.es>
2024-02-29 14:23:43 +0100

    sexta  prueba del Action: debe dejar merge

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 def suma(a: int, b: int) -> int:
     """ 
     docstring function
-    no debe dar errores en linter, 5 intento
+    no debe dar errores en linter, 6 intento
     """
     return a + b
+# End-of-file (EOF)
